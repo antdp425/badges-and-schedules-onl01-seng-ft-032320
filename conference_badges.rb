a7a7f4f6f9ae651f784be1bc318cef77f5a8b337
Anthony DiPietrantonio <antdp425@aol.com>
2020-03-24 17:13:16 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,21 @@
-# Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+  array.collect do |speaker|
+    badge_maker(speaker)
+  end
+end
+
+def assign_rooms(array)
+  room = 0
+  array.collect do |speaker|
+    room += 1
+    "Hello, #{speaker}! You'll be assigned to room #{room}!"
+  end
+end
+
+def printer(array)
+  assign_rooms(batch_badge_creator(array))
+end
